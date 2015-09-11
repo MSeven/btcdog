0498ca8e4a19fd9eb13ed974cfbdafc87d56e67d
Martin Griesbach <mgriesbach@gmail.com>
2015-09-11 21:40:17 +0200

    Refactoring: * turned script into class * switched JsonRPC library * added Ethereum Stats query.

--- config.sample.php
+++ config.sample.php
@@ -1,11 +1,17 @@
 <?php
 
 /** @var Configuration $conf Uses parse_url() array keys */
-$conf = array(
+$confBtc = array(
     'scheme' => 'http',
     'host'   => '127.0.0.1',
     'port'   => '8332',
     'user'   => 'username',
     'pass'   => 'password',
+    'path'   => '/'
+);
+$confEth = array(
+    'scheme' => 'http',
+    'host'   => 'localhost',
+    'port'   => '8545',
     'path'   => '/'
 );
--- getstats.php
+++ getstats.php
@@ -2,98 +2,140 @@
 /**
  *
  */
+use Graze\GuzzleHttp\JsonRpc\Client as RpcClient;
 use League\StatsD\Client as Statsd;
 
 require('vendor/autoload.php');
 require('config.php');
 
-define('CACHE_FILE', 'transfers.cache');
 
-
-function unparseUrl($urlParts)
+class btcDog
 {
-    $scheme = isset($urlParts['scheme']) ? $urlParts['scheme'] . '://' : '';
-    $host   = isset($urlParts['host']) ? $urlParts['host'] : '';
-    $port   = isset($urlParts['port']) ? ':' . $urlParts['port'] : '';
-    $user   = isset($urlParts['user']) ? $urlParts['user'] : '';
-    $pass   = isset($urlParts['pass']) ? ':' . $urlParts['pass'] : '';
-    $pass   = ($user || $pass) ? "$pass@" : '';
-    $path   = isset($urlParts['path']) ? $urlParts['path'] : '';
+    const BTC_CACHE_FILE = 'btc_transfers.cache';
+    const ETH_CACHE_FILE = 'eth_transfers.cache';
 
-    return $scheme . $user . $pass . $host . $port . $path;
-}
+    private $dd;
+    private $requestId;
 
-/**
- * Generic Request Wrapper.
- *
- * @param $method
- * @param $conf
- *
- * @return mixed
- */
-function doRequest($method, $conf)
-{
-    static $requestId = 0;
-    $requestId++;
-    $url      = unparseUrl($conf);
-    $response = (new JsonRpcCurl())
-        ->setUrl($url)// server url with gateway path
-        ->setId($requestId)// request ID (important for batch/async)
-        ->setMethod($method)// requested service
-        ->send();
-    return $response;
-}
+    public function __construct()
+    {
+        /**
+         * Init StatsD Client.
+         */
+        $this->dd        = new Statsd();
+        $this->requestId = 0;
+    }
 
-/**
- * Helper to only export a Gauge when the array key is present in the data.
- *
- * @param        $name
- * @param        $key
- * @param array  $response
- * @param Statsd $dd
- */
-function writeGauge($name, $key, array $response, Statsd $dd)
-{
-    if (array_key_exists($key, $response)) {
-        $dd->gauge($name, $response[$key]);
-        return true;
-    } else {
-        echo "Key " . $key . " not found in result\n";
+    function unparseUrl($urlParts)
+    {
+        $scheme = isset($urlParts['scheme']) ? $urlParts['scheme'] . '://' : '';
+        $host   = isset($urlParts['host']) ? $urlParts['host'] : '';
+        $port   = isset($urlParts['port']) ? ':' . $urlParts['port'] : '';
+        $user   = isset($urlParts['user']) ? $urlParts['user'] : '';
+        $pass   = isset($urlParts['pass']) ? ':' . $urlParts['pass'] : '';
+        $pass   = ($user || $pass) ? "$pass@" : '';
+        $path   = isset($urlParts['path']) ? $urlParts['path'] : '';
+
+        return $scheme . $user . $pass . $host . $port . $path;
+    }
+
+    /**
+     * Generic Request Wrapper.
+     *
+     * @param $method
+     * @param $conf
+     *
+     * @return mixed
+     */
+    function doRequest($method, $conf, $params = array())
+    {
+        $this->requestId++;
+        $url    = $this->unparseUrl($conf);
+        $client = RpcClient::factory($url);
+        return $client->send($client->request($this->requestId, $method, $params))->getRpcResult();
+    }
+
+    /**
+     * Helper to only export a Gauge when the array key is present in the data.
+     *
+     * @param        $name
+     * @param        $key
+     * @param array  $response
+     *
+     * @return bool
+     */
+    function writeGauge($name, $key, array $response)
+    {
+        if (array_key_exists($key, $response)) {
+            $this->dd->gauge($name, $response[$key]);
+            return true;
+        } else {
+            echo "Key " . $key . " not found in result\n";
+        }
+    }
+
+    function ethGaugeRequest($method, $conf, $params = array())
+    {
+        $response = $this->doRequest($method, $conf, $params);
+        if (!empty($response)) {
+            $this->dd->gauge('eth.' . $method, hexdec($response));
+        }
+    }
+
+    /**
+     * @param $conf
+     */
+    public function mainBtc($conf)
+    {
+        $response = $this->doRequest('getinfo', $conf);
+
+        if (is_array($response)) {
+            $this->writeGauge('btc.connections', 'connections', $response);
+            $this->writeGauge('btc.difficulty', 'difficulty', $response);
+            $this->writeGauge('btc.blockcount', 'blocks', $response);
+        }
+
+        $response = $this->doRequest('getnettotals', $conf);
+
+        if (is_array($response)) {
+            $this->writeGauge('btc.net.in', 'totalbytesrecv', $response);
+            $this->writeGauge('btc.net.out', 'totalbytessent', $response);
+
+            if (file_exists(self::BTC_CACHE_FILE)) {
+
+                $lastData = unserialize(file_get_contents(self::BTC_CACHE_FILE));
+
+                // divide by 1000 to get seconds instead of milliseconds.
+                $timeDiff = ($response['timemillis'] - $lastData['timemillis']) / 1000;
+                $inDiff   = $response['totalbytesrecv'] - $lastData['totalbytesrecv'];
+                $outDiff  = $response['totalbytessent'] - $lastData['totalbytessent'];
+
+                $this->dd->gauge('btc.net.in_sec', $inDiff / $timeDiff);
+                $this->dd->gauge('btc.net.out_sec', $outDiff / $timeDiff);
+            }
+
+            file_put_contents(self::BTC_CACHE_FILE, serialize($response));
+
+        }
+    }
+
+    public function mainEth($conf)
+    {
+
+        $this->ethGaugeRequest('net_peerCount', $conf);
+        $this->ethGaugeRequest('eth_gasPrice', $conf);
+
+        $response = $this->doRequest('eth_getBlockByNumber',$conf,array('latest',false));
+
+        $this->dd->gauge('eth.latestblock' , hexdec($response['number']));
+        $this->dd->gauge('eth.difficulty' , hexdec($response['difficulty']));
+        $this->dd->gauge('eth.totalDifficulty' , hexdec($response['totalDifficulty']));
+        $this->dd->gauge('eth.gasLimit' , hexdec($response['gasLimit']));
+        $this->dd->gauge('eth.transactionCount' , count($response['transactions']));
+        $this->dd->gauge('eth.uncleCount' , count($response['uncles']));
     }
 }
 
-/**
- * Init StatsD Client.
- */
-$dd = new Statsd();
-
-$response = doRequest('getinfo', $conf);
-
-if (is_array($response)) {
-    writeGauge('btc.connections', 'connections', $response, $dd);
-    writeGauge('btc.difficulty', 'difficulty', $response, $dd);
-    writeGauge('btc.blockcount', 'blocks', $response, $dd);
-}
-
-$response = doRequest('getnettotals', $conf);
-
-if (is_array($response)) {
-    writeGauge('btc.net.in', 'totalbytesrecv', $response, $dd);
-    writeGauge('btc.net.out', 'totalbytessent', $response, $dd);
-
-    if (file_exists(CACHE_FILE)) {
-
-        $lastData = unserialize(file_get_contents(CACHE_FILE));
-
-        // divide by 1000 to get seconds instead of milliseconds.
-        $timeDiff = ($response['timemillis'] - $lastData['timemillis']) / 1000;
-        $inDiff   = $response['totalbytesrecv'] - $lastData['totalbytesrecv'];
-        $outDiff  = $response['totalbytessent'] - $lastData['totalbytessent'];
-
-        $dd->gauge('btc.net.in_sec', $inDiff / $timeDiff);
-        $dd->gauge('btc.net.out_sec', $outDiff / $timeDiff);
-    }
-
-    file_put_contents(CACHE_FILE, serialize($response));
-
-}
+$x = new btcDog();
+$x->mainBtc($confBtc);
+$x->mainEth($confEth);
