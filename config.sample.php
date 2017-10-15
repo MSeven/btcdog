964b28796e2bd059cfabc35b783149f21dc982f3
Martin Griesbach <mgriesbach@gmail.com>
2017-10-15 15:50:35 +0200

    Docker stuff

--- config.sample.php
+++ config.sample.php
@@ -1,17 +1,10 @@
 <?php
 
-/** @var Configuration $conf Uses parse_url() array keys */
-$confBtc = array(
-    'scheme' => 'http',
-    'host'   => '127.0.0.1',
-    'port'   => '8332',
-    'user'   => 'username',
-    'pass'   => 'password',
-    'path'   => '/'
-);
+/** Configuration $conf Uses parse_url() array keys */
+
 $confEth = array(
     'scheme' => 'http',
-    'host'   => 'localhost',
-    'port'   => '8545',
-    'path'   => '/'
+    'host' => 'localhost',
+    'port' => '8545',
+    'path' => '/'
 );
